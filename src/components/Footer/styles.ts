a22bcf8ee39c3055b6f291f5d42a748fc7ddbe49
Felipe Seabra <feliperoce@gmail.com>
2023-04-20 14:07:01 -0300

    feat: remove background to footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 7.5vh;
-  background: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.secundary};
   display: flex;
   align-items: center;

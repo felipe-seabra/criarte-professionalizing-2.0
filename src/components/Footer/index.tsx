507b1764fe11c9bd94e60bdf06336dc4a090044f
Felipe Seabra <feliperoce@gmail.com>
2023-04-20 10:47:42 -0300

    Initial commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <Container>Felipe S. - &copy; 2022</Container>
+    </footer>
+  );
+};
+
+export default Footer;

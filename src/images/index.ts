8de3810456ea66555a62c12238606d0672f2d4b0
Felipe Seabra <feliperoce@gmail.com>
2023-04-20 17:29:31 -0300

    feat: insert images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,11 @@
-export { default as logoWhite } from './logo-white.png';
-export { default as logoBlue } from './logo-blue.png';
+export { default as logoWhite } from './logos/logo-white.png';
+export { default as logoBlue } from './logos/logo-blue.png';
 
-export { default as bgEnfermagem } from './bg-enfermagem.jpeg';
-export { default as bgFarmacia } from './bg-farmacia.jpg';
+export { default as bgHome } from './backgrounds/bg-home.jpg';
+
+export { default as bgEnfermagem } from './backgrounds/bg-enfermagem.jpg';
+export { default as bgFarmacia } from './backgrounds/bg-farmacia.jpg';
+
+export { default as imgEnfermagem } from './courses/enfermagem.png';
+export { default as imgFarmacia } from './courses/farmacia.png';
+export { default as imgRadiologia } from './courses/radiologia.png';
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-import { bgEnfermagem } from '../../images';
+import { bgHome } from '../../images';
 
 export const Container = styled.div`
   height: 100vh;
   width: 100vw;
-  background: url(${bgEnfermagem}) no-repeat center/cover;
+  background: url(${bgHome}) no-repeat center/cover;
   display: flex;
   align-items: center;
   padding: 0 10%;

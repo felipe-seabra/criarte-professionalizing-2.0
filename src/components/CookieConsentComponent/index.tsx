c96d3d1cb251456cd697455f17d8aa1a29120333
Felipe Seabra <feliperoce@gmail.com>
2023-04-22 18:31:28 -0300

    feat: rename cookie

--- src/components/CookieConsentComponent/index.tsx
+++ src/components/CookieConsentComponent/index.tsx
@@ -11,7 +11,11 @@ const COOKIE_STYLE: React.CSSProperties = {
 
 function CookieConsentComponent(): JSX.Element {
   return (
-    <CookieConsent style={COOKIE_STYLE} buttonText="Aceitar" expires={30}>
+    <CookieConsent
+      cookieName="criarteCookie"
+      style={COOKIE_STYLE}
+      buttonText="Aceitar"
+      expires={30}>
       &#34;Este site utiliza cookies para seu adequado funcionamento, análises,
       personalização e publicidade. Para saber mais, acesse nossa{' '}
       <Link to="/politica">Política de Privacidade</Link>. Ao continuar navegando neste

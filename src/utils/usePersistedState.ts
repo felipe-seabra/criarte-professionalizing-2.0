86bc6598ec887a46db39d01de921caea18161411
Felipe Seabra <feliperoce@gmail.com>
2023-04-22 18:34:24 -0300

    feat: rename cookie theme

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -5,18 +5,16 @@ type Response<T> = [T, Dispatch<SetStateAction<T>>];
 
 function usePresistedState(key: string): Response<string> {
   const [state, setState] = useState(() => {
-    // const storageValue = localStorage.getItem(key);
     const cookies = parseCookies();
 
-    if (cookies.USER_THEME) {
-      return cookies.USER_THEME;
+    if (cookies.CRIARTE_THEME) {
+      return cookies.CRIARTE_THEME;
     }
     return key;
   });
 
   useEffect(() => {
-    // localStorage.setItem(key, JSON.stringify(state));
-    setCookie(null, 'USER_THEME', state, {
+    setCookie(null, 'CRIARTE_THEME', state, {
       maxAge: 30 * 24 * 60 * 60,
       path: '/'
     });

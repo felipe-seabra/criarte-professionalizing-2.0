90d068de5f32cbc1f3474d7e04466eb40f0413a3
Felipe Seabra <feliperoce@gmail.com>
2023-05-05 16:29:29 -0300

    feat: handle hash load

--- src/components/CardCourse/index.tsx
+++ src/components/CardCourse/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BlurhashCanvas } from 'react-blurhash';
-
 import { Container } from './styles';
 import { ICourses } from '../../interfaces';
 
@@ -9,9 +8,33 @@ function CardCourse(texts: ICourses) {
   const { id, image, title, description } = texts;
   const link = `/cursos/${id}`;
   const [loaded, setLoaded] = useState(false);
+  const [hash, setHash] = useState('');
 
   const handleImageLoad = () => {
     setLoaded(true);
+  };
+
+  const handleHashLoad = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const imageElement = event.target as HTMLImageElement;
+    const { width, height } = imageElement;
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext('2d');
+    if (context) {
+      context.drawImage(imageElement, 0, 0);
+      const imageData = context.getImageData(0, 0, width, height);
+      import('blurhash').then((blurhash) => {
+        const generateHash = blurhash.encode(
+          imageData.data,
+          imageData.width,
+          imageData.height,
+          4,
+          3
+        );
+        setHash(generateHash);
+      });
+    }
   };
 
   return (
@@ -27,13 +50,21 @@ function CardCourse(texts: ICourses) {
               alt={title}
               onLoad={handleImageLoad}
             />
-            {!loaded && (
+            {!loaded && hash && (
               <BlurhashCanvas
-                hash="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
+                hash={hash}
                 className="course-img mb-5 img-fluid"
                 width={200}
                 height={200}
                 punch={1}
+              />
+            )}
+            {!loaded && !hash && (
+              <img
+                className="course-img mb-5 img-fluid"
+                src={image}
+                alt={title}
+                onLoad={handleHashLoad}
               />
             )}
           </div>

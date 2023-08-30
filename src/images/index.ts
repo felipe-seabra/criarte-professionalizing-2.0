2040dfb1b5e2a3db5080b9422a522fde6fc2d021
Felipe Seabra <feliperoce@gmail.com>
2023-08-30 09:43:47 -0300

    feat: convert backgrount to webp

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,10 +2,7 @@ export { default as logoWhite } from './logos/logo-white.png';
 export { default as logoBlue } from './logos/logo-blue.png';
 export { default as logoC } from './logos/logo-c.png';
 
-export { default as bgHome } from './backgrounds/bg-home.jpg';
-
-export { default as bgEnfermagem } from './backgrounds/bg-enfermagem.jpg';
-export { default as bgFarmacia } from './backgrounds/bg-farmacia.jpg';
+export { default as bgHome } from './backgrounds/bg-home.webp';
 
 export { default as imgEnfermagem } from './courses/enfermagem.png';
 export { default as imgFarmacia } from './courses/farmacia.png';

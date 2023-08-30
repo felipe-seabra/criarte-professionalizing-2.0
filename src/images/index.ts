d6811e793ef9219b0139bc7ccf3495384d222fb2
Felipe Seabra <feliperoce@gmail.com>
2023-08-30 09:50:14 -0300

    feat: convert images to webp and remove png

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,9 +1,9 @@
-export { default as logoWhite } from './logos/logo-white.png';
-export { default as logoBlue } from './logos/logo-blue.png';
-export { default as logoC } from './logos/logo-c.png';
+export { default as logoWhite } from './logos/logo-white.webp';
+export { default as logoBlue } from './logos/logo-blue.webp';
+export { default as logoC } from './logos/logo-c.webp';
 
 export { default as bgHome } from './backgrounds/bg-home.webp';
 
-export { default as imgEnfermagem } from './courses/enfermagem.png';
-export { default as imgFarmacia } from './courses/farmacia.png';
-export { default as imgRadiologia } from './courses/radiologia.png';
+export { default as imgEnfermagem } from './courses/enfermagem.webp';
+export { default as imgFarmacia } from './courses/farmacia.webp';
+export { default as imgRadiologia } from './courses/radiologia.webp';

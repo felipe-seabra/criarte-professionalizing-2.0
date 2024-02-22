6142c7ee2541a8302628710565f7bb3ab183f434
Felipe Seabra <feliperoce@gmail.com>
2024-02-22 09:04:47 -0300

    feat: replace link to location

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,7 +13,7 @@ import ChatPopup from '../ChatPopup';
 
 type Position = 'absolute' | 'relative';
 
-const LOCATION = `https://www.google.com.br/maps/place/Col%C3%A9gio+Criarte/@-22.1414597,-51.3914309,17z/data=!3m1!4b1!4m6!3m5!1s0x9493f501fcbdda6f:0xe19d9a18e1e99b9e!8m2!3d-22.1414597!4d-51.3888506!16s%2Fg%2F1tk_qcc7`;
+const LOCATION = `https://maps.app.goo.gl/zkQia9pSeYLDLYPz9`;
 
 const LINK_SAENET = `http://criarte.saenet.net.br/Sae8Portal/login.aspx?ReturnUrl=%2fSae8Portal%2fprincipal.aspx`;
 
